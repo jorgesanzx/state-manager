d95d453c78d8bbb9829be59a0f54eb8648c4c91f
Jorge Sanz <mail@jorgesanz.com>
2017-03-26 18:41:28 +0200

    Prefix back-end API with /api

--- back-end/config/routes.rb
+++ back-end/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
+  scope :api do
+    post 'user_token' => 'user_token#create'
 
-  defaults format: :json do
-    resources :states
+    defaults format: :json do
+      resources :states
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
